4d593d057710749ebe1b5d3e494a1478b1f071a2
malditoprotozoo <virodriguez.fe@gmail.com>
2017-11-06 00:28:04 -0300

    JS file added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,62 @@
+var airlineSeats = [false, false, false, false, false, false, false, false, false, false];
+
+
 function bookTicket() {
-	var answer = prompt("¿En qué zona prefieres reservar tu asiento? \n 1. Primera Clase \n 2. Clase Económica");
-	if (answer !== "") {
-		if (answer === "1") {
-			firstClass();
-		} else if (answer === "2") {
-			economyClass();
-		} else {
-			alert("Esa no es una opción válida.");
-		}
-	}
-}
+    var answer = prompt("¿En qué zona prefieres reservar tu asiento? \n 1. Primera Clase \n 2. Clase Económica \n Por favor, ingresa el número");
+    if (answer !== "") {
+        if (answer === "1") {
+            firstClass();
+        } else if (answer === "2") {
+            economyClass();
+        } else {
+            alert("Esa no es una opción válida.");
+        }
+    }
+};
 
 function firstClass() {
+	for (var i = 0; i < 4; i++) {
+		if (airlineSeats[i] === false) {
+			airlineSeats[i] = true;
+			document.getElementsByTagName("td")[i].setAttribute("class", "occupied");
+			break;
+		} else if (i == 3 && airlineSeats[i] === true) {
+			reasignEconomyClass();
+		}
+	}
+};
 
-}
+function economyClass() {
+	for (var i = 4; i < 10; i++) {
+		if (airlineSeats[i] === false) {
+			airlineSeats[i] = true;
+			document.getElementsByTagName("td")[i].setAttribute("class", "occupied");
+			break;
+		} else if (i == 9 && airlineSeats[i] === true) {
+			reasignFirstClass();
+		}
+	}
+};
+
+function reasignEconomyClass() {
+	var reasign = confirm("Ya no quedan asientos disponibles en Primera Clase. ¿Te gustaría reservar un asiento en la Clase Económica?");
+	if (reasign === true) {
+		economyClass();
+	} else {
+		nextFlight();
+	}
+};
+
+function reasignFirstClass() {
+	var reasign = confirm("Ya no quedan asientos disponibles en Clase Económica. ¿Te gustaría reservar un asiento en la Primera Clase?");
+	if (reasign === true) {
+		economyClass();
+	} else {
+		nextFlight();
+	}
+};
+
+function nextFlight() {
+	alert("Nuestro próximo vuelo sale en 3 horas :)")
+};
+
